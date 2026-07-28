00d0454d212103f2c76e930c22614fce802a2dea
Soham Bhoir <soham4765@gmail.com>
2026-07-28 19:07:00 +0530

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
+import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
@@ -48,5 +49,13 @@ def evaluate_models(X_train, Y_train, X_test, Y_test, models, param):
             best_models[model_name] = best_model
 
         return report, best_models
+    except Exception as e:
+        raise CustomException(e, sys)
+        
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
     except Exception as e:
         raise CustomException(e, sys)
